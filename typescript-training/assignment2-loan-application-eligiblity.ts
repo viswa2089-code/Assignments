529a05b01359d7cba89934e2e6b48184bdaf86d8
viswa2089-code <viswa2089@gmail.com>
2026-06-25 23:59:03 +0530

    assignment2-loan-application-eligiblity.ts

--- typescript-training/assignment2-loan-application-eligiblity.ts
+++ typescript-training/assignment2-loan-application-eligiblity.ts
@@ -1,6 +1,23 @@
+//Create common function and then based on below details, print whether user is eligible to get the loan or not
+// A bank evaluates loan applicants based on the following criteria:
+// 1. Credit Score:
+// o If the credit score is above 750, the loan is automatically approved.
+// o If the credit score is between 650 and 750, additional checks are performed.
+// o If the credit score is below 650, the loan is denied.
+// 2. Income:
+// o For credit scores between 650 and 750, the customer’s income must be at least $50,000
+// for the loan to be considered.
 
+// 3. Employment Status:
+// o If the customer’s income is at least 50,000, the system checks whether the customer is
+// employed.
+// o If the customer is unemployed, the loan is denied.
+// 4. Debt-to-Income Ratio:
+// o If the customer is employed, the system checks the debt-to-income (DTI) ratio.
+// o If the DTI ratio is less than 40%, the loan is approved.
+// o If the DTI ratio is 40% or greater, the loan is denied.
 
-
+//Logic starts from here....
 
 let customerName:string = "John Doe";
 let creditScore:number = 750;
